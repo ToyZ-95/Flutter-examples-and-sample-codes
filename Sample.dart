7b247036d89f8b7421f386483c49b65102267903
Asad <62109756+ToyZ-95@users.noreply.github.com>
2021-05-10 11:22:38 +0530

    Created Sample.dart
    Sample of asynchronous programming in Dart/Flutter for future reference

---
+++ Sample.dart
@@ -0,0 +1,66 @@
+/********************************************** async await  ************************************************************************/
+
+import "dart:async";
+
+void main() {
+  print('Program start');
+ pintFileContent();
+  print('Program end');
+  
+}
+
+
+void pintFileContent() async{
+  
+   String fileContent = await downloadFile();
+  print('The content of file is $fileContent');
+  
+}
+
+ Future <String> downloadFile(){
+  
+  Future<String> result = Future.delayed(Duration(seconds:6),(){
+    return "My secret file content";
+  });
+  
+  return result;
+  
+}
+
+/**********************************************************************************************************************/
+
+/********************************************** Future API  ************************************************************************/
+
+import "dart:async";
+
+void main() {
+  print('Program start');
+ pintFileContent();
+  print('Program end');
+  
+}
+
+
+void pintFileContent() {
+  
+  Future<String> fileContent =  downloadFile();
+  
+  fileContent.then((resultedString) {
+    print('The content of file is $resultedString');
+  });
+  
+}
+
+ Future <String> downloadFile(){
+  
+  Future<String> result = Future.delayed(Duration(seconds:6),(){
+    return "My secret file content";
+  });
+  
+  return result;
+  
+}
+
+
+
+/**********************************************************************************************************************/
